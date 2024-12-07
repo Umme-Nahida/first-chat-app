a23cfa8a0ae5adaf558c5fc81edd25e30624a2bc
Developer Nahida <ummenahida20@gmail.com>
2024-12-08 00:57:44 +0600

    create my owon chat app

--- real-chatting/src/Pages/Chat.jsx
+++ real-chatting/src/Pages/Chat.jsx
@@ -7,7 +7,7 @@ const Chat = () => {
             <div className='w-[400px] mx-auto min-h-20 flex flex-col items-center justify-between border border-gray-300 bg-gray-300'>
                 <div className="chat-head w-full bg-blue-600 text-white text-xl flex items-center justify-between pl-5">
                     <div className="room">sports  </div>
-                    <button className='py-2 px-2 bg-emerald-400'>X</button>
+                    <button className='py-2 px-2 bg-emerald-400'>XX</button>
                 </div>
                 <div className="chat-box w-full ">
                     <input  type="text" name="message"  className='px-3 py-2 w-full focus:outline-dotted border border-black ' placeholder='message' />
